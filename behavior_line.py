80f26af6a524854ef403c84a02b991b088ef070f
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-08-24 17:04:56 +0200

    motors hardware abstraction layer

--- behavior_line.py
+++ behavior_line.py
@@ -3,8 +3,6 @@ from Raspi_MotorHAT import Raspi_MotorHAT as rpi_mh
 from time import sleep
 
 r = robot.Robot()
-r.left_motor.setSpeed(r.convert_speed(80))
-r.right_motor.setSpeed(r.convert_speed(80))
-r.left_motor.run(rpi_mh.FORWARD)
-r.right_motor.run(rpi_mh.FORWARD)
+r.set_left(-80)
+r.set_right(-80)
 sleep(1)
--- robot.py
+++ robot.py
@@ -5,19 +5,34 @@ import atexit
 
 class Robot:
     def __init__(self, motorhat_addr=0x60):
-        # motor driver i2c adress setup
+
         self._mh = rpi_mh(addr=motorhat_addr)
 
-        # motors init
         self.left_motor = self._mh.getMotor(1)
         self.right_motor = self._mh.getMotor(2)
 
-        # stop
         atexit.register(self.stop_motors)
 
     def convert_speed(self, speed):
-        return (speed * 255) // 100
+        mode = rpi_mh.RELEASE
+        if speed > 0:
+            mode = rpi_mh.FORWARD
+        elif speed < 0:
+            mode = rpi_mh.BACKWARD
+
+        output_speed = (abs(speed*255)) // 100
+        return mode, int(output_speed)
 
     def stop_motors(self):
         self.left_motor.run(rpi_mh.RELEASE)
         self.right_motor.run(rpi_mh.RELEASE)
+
+    def set_left(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.left_motor.setSpeed(output_speed)
+        self.left_motor.run(mode)
+
+    def set_right(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.right_motor.setSpeed(output_speed)
+        self.right_motor.run(mode)
