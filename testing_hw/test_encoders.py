ea0e16241bf70bb07627a4ab32438cc66aff22d7
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-08-30 16:31:57 +0200

    testing encoders - lil fix

--- testing_hw/test_encoders.py
+++ testing_hw/test_encoders.py
@@ -28,5 +28,5 @@ bot.set_left(90)
 bot.set_right(90)
 
 while time.time() < stop_at_time:
-    logger.info(f"Left: Right: {right_encoder.pulse_count}")
+    logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
     time.sleep(0.05)
