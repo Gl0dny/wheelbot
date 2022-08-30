a830bcc7f17b0a5e9e7e0a14d74661a9084580dd
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-08-30 15:52:25 +0200

    encodercounter class

--- testing_hw/test_encoders.py
+++ testing_hw/test_encoders.py
@@ -3,5 +3,18 @@ import time
 import logging
 from gpiozero import DigitalInputDevice
 
-logger = logging.getLogger("test_encoders"
-)
+logger = logging.getLogger("test_encoders")
+
+class EncoderCounter(object):
+    def __init__(self, pin_number):
+        self.pulse_count = 0
+        self.device = DigitalInputDevice(pin=pin_number)
+        self.device.pin.when_changed = self.when_changed
+
+    def when_changed(self, time_ticks, state):     
+        self.pulse_count += 1
+
+
+bot = Robot()
+left_encoder = EncoderCounter(4)
+right_encoder = EncoderCounter(26)
