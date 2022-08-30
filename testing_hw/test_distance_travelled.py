aa65e39121f3870fea2beb5bf1f2693b92c81de9
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-08-30 17:08:41 +0200

    encoders output in mm

--- testing_hw/test_distance_travelled.py
+++ testing_hw/test_distance_travelled.py
@@ -1,10 +1,11 @@
-from json import encoder
+import sys
+sys.path.insert(0, '/home/pi/Projects/2-wheeled-autonomous-robot')
 from robot import Robot
 import time
 import math
 import logging
 
-logger = logging.getLogger({"test_distance_travelled"})
+logger = logging.getLogger("test_distance_travelled")
 
 wheel_diameter_mm = 70.0
 encoders_shield_slits = 20.0
@@ -14,3 +15,17 @@ ticks_to_mm_const = (math.pi * wheel_diameter_mm) / tick_per_revolution
 
 def ticks_to_mm(ticks):
     return int(ticks_to_mm_const * ticks)
+
+bot = Robot()
+stop_at_time = time.time() + 1
+
+logging.basicConfig(level=logging.INFO)
+bot.set_left(90)
+bot.set_right(90)
+
+while time.time() < stop_at_time:
+    logger.info("Left: {} [mm] Right: {} [mm]".format(
+        ticks_to_mm(bot.left_encoder.pulse_count),
+        ticks_to_mm(bot.right_encoder.pulse_count)))
+    time.sleep(0.1)
+    
