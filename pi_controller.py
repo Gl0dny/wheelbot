54790d66ecd8eb681c1eeafbf2312c0a4e3dcde9
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-08-31 15:13:24 +0200

    P and I elements methods

--- pi_controller.py
+++ pi_controller.py
@@ -3,7 +3,13 @@ import logging
 logger = logging.getLogger("pi_controller")
 
 class PIController:
-def __init__(self, proportional_constant=0, integral_constant=0):
-    self.proportional_constant = proportional_constant
-    self.integral_constant = integral_constant
-    self.integral_sum = 0
+    def __init__(self, proportional_constant=0, integral_constant=0):
+        self.proportional_constant = proportional_constant
+        self.integral_constant = integral_constant
+        self.integral_sum = 0
+    def handle_proportional(self, error):
+        return self.proportional_constant * error
+
+    def handle_integral(self, error):
+        self.integral_sum += error
+        return self.integral_constant * self.integral_sum
