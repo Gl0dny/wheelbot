3cc046b28af336508c5dff1b411dd7488b225c0c
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-09-03 18:52:50 +0200

    drive_distance working

--- drive_distance.py
+++ drive_distance.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import atexit
 from pid_controller import PIController
 from robot import Robot, EncoderCounter
 import time
@@ -16,16 +17,20 @@ def drive_distance(bot, distance, speed=80):
     set_primary_motor(speed)
     set_secondary_motor(speed)
 
-    while primary_encoder.pulse_count < distance or secondary_encoder.pulse_coutn < distance:
+    while primary_encoder.pulse_count < distance or secondary_encoder.pulse_count < distance:
         time.sleep(0.01)
         error = primary_encoder.pulse_count - secondary_encoder.pulse_count
         adjustment = controller.get_value(error)
         set_primary_motor(int(speed - adjustment))
         set_secondary_motor(int(speed + adjustment))
 
-        logger.debug(f"""Encoders: primary: {primary_encoder.pulse_count},
-                    secondary: {secondary_encoder.pulse_count},
-                    error: {error}, adjustment: {adjustment:.2f}\n""")
+        logger.debug(f"Encoders: primary: {primary_encoder.pulse_count}, secondary: {secondary_encoder.pulse_count},\nerror: {error}, adjustment: {adjustment:.2f}\n")
+        logger.info(f"Distances: primary: {primary_encoder.distance_in_mm()} [mm], secondary: {secondary_encoder.distance_in_mm()} [mm]\n")
 
-    
+logging.basicConfig(level=logging.DEBUG)
+bot = Robot()
+distance_to_drive = 1000
+distance_in_ticks = EncoderCounter.mm_to_ticks(distance_to_drive)
+drive_distance(bot, distance_in_ticks) 
+
 
