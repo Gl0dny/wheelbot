0b0180da50af2dab13da1ce155847a13dfac6550
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-09-03 18:25:41 +0200

    drive distance function

--- drive_distance.py
+++ drive_distance.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from pid_controller import PIController
+from robot import Robot, EncoderCounter
+import time
+import logging
+
+logger = logging.getLogger("drive_distance")
+
+def drive_distance(bot, distance, speed=80):
+    set_primary_motor = bot.set_left
+    primary_encoder = bot.left_encoder
+    set_secondary_motor = bot.set_right
+    secondary_encoder = bot.right_encoder
+
+    controller = PIController(proportional_constant=4, integral_constant=0.3)
+    set_primary_motor(speed)
+    set_secondary_motor(speed)
