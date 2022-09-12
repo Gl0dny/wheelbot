bd17ba8abe413b6b38ba86ee28f14cf2ae5134c0
Gl0dny_rpi_1 <krystian.glodek1717@gmail.com>
2022-09-12 11:27:13 +0200

    camera_stream

--- camera_stream.py
+++ camera_stream.py
@@ -9,7 +9,19 @@ encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 def setup_camera():
     camera = PiCamera()
     camera.resolution = size
-    canera0franerate = 30
+    cameraframerate = 30
     camera.rotation = 180
     return camera
+
+def start_stream(camera):
+    image_storage = PiRGBArray(camera, size=size)
+    cam_stream = camera.capture_continous(image_storage, format="bgr", use_video_port = True)
+    
+def get_encoded_bytes_for_frame(frame):
+    result, encoded_image = cv2.imencode('.jpg', frame, encode_param)
+    return encoded_image.tostring()
+
+for raw_frame in cam_stream:
+    yield raw_frame.array
+    image.storage.truncate(0)
 
