a71b8642af862e1f6823ab31958a4997c033bee5
Gl0dny <krystian.glodek1717@gmail.com>
2022-09-13 23:05:49 +0200

    WORKING XDD fucking typo

--- p_2war_hardware_abstraction_layer/camera_stream.py
+++ p_2war_hardware_abstraction_layer/camera_stream.py
@@ -9,14 +9,13 @@ encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 def setup_camera():
     camera = PiCamera()
     camera.resolution = size
-    camera.framerate = 30
+    camera.framerate = 24
     camera.rotation = 180
     return camera
 
 def start_stream(camera):
     image_storage = PiRGBArray(camera, size=size)
-    cam_stream = camera.capture_continous(image_storage, format="bgr", use_video_port = True)
-    
+    cam_stream = camera.capture_continuous(image_storage, format="bgr", use_video_port=True)
     for raw_frame in cam_stream:
         yield raw_frame.array
         image_storage.truncate(0)
