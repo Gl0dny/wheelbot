08620cd2f3203de27b94180af4663a2d6e294851
Gl0dny <krystian.glodek1717@gmail.com>
2022-09-14 16:34:33 +0200

    controlled server behavior example

--- control_image_behavior.py
+++ control_image_behavior.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+from ssl import _PasswordType
 import time
+from tkinter import NONE
+from xml.dom.minidom import parseString
 import p_2war_hardware_abstraction_layer as HAL
 import image_app_core as img_server
 
@@ -10,3 +13,14 @@ def controlled_image_server_behavior():
     for frame in HAL.camera_stream.start_stream(camera):
         encoded_bytes = HAL.camera_stream.get_encoded_bytes_for_frame(frame)
         img_server.put_output_image(encoded_bytes)
+
+        instruction = img_server.get_control_instruction()
+        if instruction and instruction['command'] == "exit":
+            print("Closing...")
+            pass
+
+    process = img_server.start_server_process('control_image_behavior.html')
+    try:
+        controlled_image_server_behavior()
+    finally:
+        process.terminate()
