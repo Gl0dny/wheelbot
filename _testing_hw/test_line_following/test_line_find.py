0f8abdc6418472976e33dcd593638e7d15049d4e
Gl0dny <krystian.glodek1717@gmail.com>
2022-09-17 13:48:48 +0200

    blurred test

--- _testing_hw/test_line_following/test_line_find.py
+++ _testing_hw/test_line_following/test_line_find.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -5,10 +6,18 @@ from matplotlib import pyplot as plt
 img = cv2.imread("line1.jpg")
 assert img is not None, "Cannot read the img"
 resized_img = cv2.resize(img, (320, 240))
-gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
-row = gray[180].astype(np.int32)
+gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.blur(gray, (3,3))
+
+# row = gray[180].astype(np.int32)
+# diff = np.diff(row)
+# x = np.arange(len(diff))
+# plt.plot(x, diff)
+# plt.savefig("not_blurred.png")
+
+row = blurred[180].astype(np.int32)
 diff = np.diff(row)
 x = np.arange(len(diff))
 plt.plot(x, diff)
-plt.savefig("not_blurred.png")
+plt.savefig("blurred.png")
