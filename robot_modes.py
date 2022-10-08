709464ce0a144f29068d9b62603d7c9b2bb783bc
Gl0dny <krystian.glodek1717@gmail.com>
2022-10-08 19:35:43 +0200

    robot modes

---
+++ robot_modes.py
@@ -0,0 +1,27 @@
+import subprocess
+
+class RobotModes():
+    mode_config = {
+        "avoid_behavior": "avoid_behavior.py",
+        "circle_head": "test_circle_pan_tilt_behavior.py",
+        "test_rainbow": "test_rainbow.py"
+    }
+
+    def __init__(self):
+        self.current_process = None
+
+    def is_running(self):
+        return self.current_process and self.current_process.returncode is None
+
+    def run(self, mode_name):
+        if not self.is_running():
+            script = self.mode_config[mode_name]
+            self.current_process = subprocess.Popen(["python3", script])
+            return True
+        return False
+
+    def stop(self):
+        if self.is_running():
+            self.current_process.send_signal(subprocess.signal.SIGINT)
+            self.current_process = None
+
